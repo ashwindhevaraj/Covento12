632d4f96a6202a22a2f96cec831fcfef0db073d3
aswindevaraj <asdev@vestas.com>
2023-10-14 17:29:30 +0530

    Newlearning1

---
+++ coventotest1/src/test/java/Testcases/TC_0001_checkforlogin.java
@@ -0,0 +1,11 @@
+package Testcases;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class TC_0001_checkforlogin {
+	@Test
+	public void check1() {
+System.out.println("HI i have installed TestNG now");
+}
+}
